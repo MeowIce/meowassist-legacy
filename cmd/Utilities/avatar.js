7ac9e3dbb84d39ab7729efb174a70818a7d2b21b
Jason <bennguyen20122006@gmail.com>
2022-07-04 14:44:26 +0700

    fixed avatar command

--- cmd/Utilities/avatar.js
+++ cmd/Utilities/avatar.js
@@ -1,8 +1,32 @@
-const { MessageEmbed } = require("discord.js");
-module.exports = {
+const Discord = require("discord.js");
+
+/**
+ * @typedef CallbackObject
+ * @property {Discord.CommandInteraction} interaction
+ * @property {Discord.Client} client
+ * @property {Discord.Guild} guild
+ * @property {Discord.GuildMember} member
+ * @property {Discord.User} user
+ * @property {Discord.CommandInteractionOptionResolver} options
+ */
+
+/**
+ * @typedef CommandOptions
+ * @property {Discord.ApplicationCommandData | Discord.ApplicationCommandSubCommandData | Discord.ApplicationCommandSubGroupData} data
+ * @property {boolean} [ownerOnly]
+ * @property {boolean} [wholeCommand]
+ * @property {Discord.PermissionString[]} [permissions]
+ * @property {Discord.PermissionString[]} [clientPermissions]
+ * @property {(obj: CallbackObject) => any} callback
+ */
+
+/**
+ * @type {CommandOptions}
+ */
+const commandBase = {
 	data: {
 		name: "avatar",
-		description: "To get a user's avatar",
+		description: "To get a user's avatar.",
 		options: [
 			{
 				name: "user",
@@ -12,14 +36,10 @@ module.exports = {
 			},
 		],
 	},
-	wholeCommand: false,
-	callback: async ({ interaction, options, user }) => {
-		/**
-		 * @type {import('discord.js').User}
-		 */
+	callback: async ({ interaction, client, guild, member, user, options }) => {
 		const targetUser = options.getUser("user") || user;
 
-		const embed = new MessageEmbed()
+		const embed = new Discord.MessageEmbed()
 			.setColor("WHITE")
 			.setTitle(`Here is ${targetUser.username}'s thumbnail.`)
 			.setImage(targetUser.displayAvatarURL({ dynamic: true }));
@@ -29,3 +49,5 @@ module.exports = {
 		});
 	},
 };
+
+module.exports = commandBase;

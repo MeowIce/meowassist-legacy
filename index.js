80c136cc1c79d0584eea9a9ede4fd43d1938d292
Jason <bennguyen20122006@gmail.com>
2022-07-04 14:51:29 +0700

    fixed listen

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const discord = require("discord.js");
 const config = require("./config.json");
 const client = new discord.Client({ intents: 32767 });
 const loader = require("./loader");
+const handler = require("./handler");
 
 client.on("ready", async () => {
 	client.user.setPresence({
@@ -11,7 +12,7 @@ client.on("ready", async () => {
 	console.log(`Đăng nhập vào ${client.user.tag}.`);
 
 	await loader(client);
-
 	await loader.feature(client);
+	await handler.listen(client);
 });
 client.login(config.token);

75a95f3c4112e0d6a6f5006e7de6d26457a3492e
MeowIce <nguyenanhuy2208@gmail.com>
2022-07-04 23:44:12 +0700

    Added userinfo.js

---
+++ cmd/Utilities/userinfo.js
@@ -0,0 +1,84 @@
+const { Discord, MessageEmbed } = require("discord.js");
+const moment = require("moment");
+
+/**
+ * @typedef CallbackObject
+ * @property {Discord.CommandInteraction} interaction
+ * @property {Discord.Client} client
+ * @property {Discord.Guild} guild
+ * @property {Discord.GuildMember} member
+ * @property {Discord.User} user
+ * @property {Discord.CommandInteractionOptionResolver} options
+ */
+
+/**
+ * @typedef CommandOptions
+ * @property {Discord.ApplicationCommandData | Discord.ApplicationCommandSubCommandData | Discord.ApplicationCommandSubGroupData} data
+ * @property {boolean} [owners]
+ * @property {boolean} [wholeCommand]
+ * @property {Discord.PermissionString[]} [permissions]
+ * @property {Discord.PermissionString[]} [clientPermissions]
+ * @property {(obj: CallbackObject) => any} callback
+ */
+
+/**
+ * @type {CommandOptions}
+ */
+ const commandBase = {
+	data: {
+		name: "userinfo",
+		description: "Lấy thông tin người dùng.",
+        options: [
+            {
+                name: "target",
+                type: "USER",
+                description: "Đối tượng...",
+                required: false,
+            }
+    ]
+	},
+    wholeCommand: true,
+    callback: async ({ interaction, client, guild, member, user, options }) => {
+        const locale = require("moment/locale/vi")
+        const usr = interaction.options.getUser('target') || interaction.user || interaction.guild.members.fetch(interaction.targetId);
+        let rolemap = interaction.guild.roles.cache
+        .sort((a, b) => b.position - a.position)
+        .map(r => r)
+        .join(",");
+        if (rolemap.length > 1024) rolemap = "Quá nhiều roles để hiển thị !";
+        if (!rolemap) rolemap = "Người dùng không có role nào.";
+
+
+        const embed = new MessageEmbed()
+            .setAuthor(`${usr.tag}`, usr.displayAvatarURL())
+            .setThumbnail(`${usr.displayAvatarURL({ dynamic: true, size: 512})}`)
+            .addFields(
+                {
+                    name: `Ping`,
+                    value: `<@${usr.id}>`
+                }
+            )
+            .addFields(
+                {
+                    name: `ID người dùng`,
+                    value: `${usr.id}`
+                }
+            )
+            .addFields(
+                {
+                    name: `Đã vào Discord lúc`,
+                    // Convert output thằng này qua tiếng Việt:
+                    value: `${moment(usr.createdAt).format('LLLL')}`
+                }
+            )
+            .addFields(
+                {
+                    name: `Vai trò`,
+                    value: rolemap
+                }
+            )
+            .setFooter(`Lệnh được thực thi bởi ${interaction.user.username}`)
+        return interaction.reply({ embeds: [embed] })
+    }
+ }
+ module.exports = commandBase;

1f2ade7528d781753ee7668fdae7313de3e9f636
MeowIce <nguyenanhuy2208@gmail.com>
2022-07-08 20:08:28 +0700

    Upgraded startup verbose

--- index.js
+++ index.js
@@ -4,21 +4,26 @@ const client = new discord.Client({ intents: 32767 });
 const loader = require("./loader");
 const handler = require("./handler");
 const mongoose = require("mongoose");
+const moment = require("moment");
 
 client.on("ready", async () => {
 	client.user.setPresence({
 		activities: [{ name: "/help", type: "LISTENING" }],
 		status: "online",
 	});
-	console.log(`Đăng nhập vào ${client.user.tag}.`);
+	console.log(`Logged into ${client.user.tag}.`);
 
 	await mongoose
 		.connect(config.mongoUrl)
-		.then(() => console.log(`Successfully connected to DB!`))
+		.then(() => console.log(`Successfully connected to DB !`))
 		.catch(console.log);
 
 	await loader(client);
 	await loader.feature(client);
 	await handler.listen(client);
+	startDate = new Date();
+	console.log("Bot started up at", moment(startDate).format(
+		"LLLL."
+	))
 });
 client.login(config.token);

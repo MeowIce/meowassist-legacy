6b96b0a61ca2f8136736376386b9e78cdeb89a9f
MeowIce <nguyenanhuy2208@gmail.com>
2022-07-09 14:33:19 +0700

    Fixed emit.js permissions

--- cmd/Developer/emit.js
+++ cmd/Developer/emit.js
@@ -45,17 +45,28 @@ const commandBase = {
 		],
 	},
 	wholeCommand: true,
-	owners: true,
+	//owners: true,
 	callback: async ({ interaction, client, guild, member, user, options }) => {
 		const subcommand = options.getSubcommand();
+		const config = require("../../config.json");
+		if (interaction.user.id == config.owners) {
+			client.emit(EVENTS[subcommand], member);
 
-		client.emit(EVENTS[subcommand], member);
-
-		return await interaction.reply({
-			content: "Thành công!",
-			ephemeral: true,
-		});
-	},
-};
+			return await interaction.reply({
+				content: "Thành công!",
+				ephemeral: true,
+		})
+			const d = new Date();
+			console.log(interaction.user.tag, "executed command", commandBase.data.name, "at",`${d.getDate()}/${d.getMonth()}/${d.getFullYear()} - ${d.getHours()}:${d.getMinutes()}` )
+		}
+		else {
+			console.log(interaction.user.tag, "tried to execute", commandBase.data.name, "but failed because he has no permission.")
+			return await interaction.reply({
+				content: ":no_entry_sign: Bạn không phải là developer để sử dụng lệnh này !",
+				ephemeral: true,
+			});
+		};
+	}
+	};
 
 module.exports = commandBase;

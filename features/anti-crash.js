f23dc60d486029547b370abc74fd4309ba0cdb61
Jason <bennguyen20122006@gmail.com>
2022-07-09 16:13:39 +0700

    fixed everything in Developer group

---
+++ features/anti-crash.js
@@ -0,0 +1,33 @@
+const Discord = require("discord.js");
+
+/**
+ *
+ * @param {Discord.Client} client
+ */
+module.exports = (client) => {
+	process.on("uncaughtException", (error) => {
+		console.log(error.stack);
+	});
+
+	process.on("uncaughtExceptionMonitor", (error) => {
+		console.log(error.stack);
+	});
+
+	process.on("unhandledRejection", (error) => {});
+
+	process.on("beforeExit", (code) => {
+		console.log(code);
+	});
+
+	process.on("exit", (code) => {
+		console.log(code);
+	});
+
+	process.on("multipleResolves", (type, promise, reason) => {
+		console.log(reason);
+	});
+
+	client.on("error", (error) => {
+		console.log(error.stack);
+	});
+};

14b97759ecdea1035d3f81c7487ef230f90baec7
MeowIce <nguyenanhuy2208@gmail.com>
2022-07-09 19:54:46 +0700

    Added Contributor(s)

--- cmd/About/about.js
+++ cmd/About/about.js
@@ -52,6 +52,10 @@ const commandBase = {
 					value: `<@${config.owners}>`,
 				},
 				{
+					name: `Hỗ trợ phát triển bot:`,
+					value: `<@${config.collab}>`,
+				},
+				{
 					name: `Ngày tạo bot:`,
 					value: `<t:${moment(client.user.createdTimestamp)
 						.tz("Asia/Ho_Chi_Minh")

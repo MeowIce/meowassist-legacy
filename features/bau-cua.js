3626d2838aad5d0946a1898a05e62b95e568237b
Jason <bennguyen20122006@gmail.com>
2022-07-10 14:25:35 +0700

    added economy system and baucua.js game

---
+++ features/bau-cua.js
@@ -0,0 +1,425 @@
+const Discord = require("discord.js");
+const { addMoney, removeMoney, getMoney } = require("./economy");
+
+module.exports = () => {};
+
+class BauCua {
+	/**
+	 * @typedef {'nai' | 'bầu' | 'gà' | 'cá' | 'cua' | 'tôm'} Animal
+	 */
+
+	/**
+	 * @typedef Bet
+	 * @property {Discord.Snowflake} id
+	 * @property {number} money
+	 * @property {boolean} haveBeenPaidBack
+	 * @property {boolean} win
+	 * @property {Animal} bettedFor
+	 */
+
+	/**
+	 * @typedef RollResult
+	 * @property {number} number
+	 * @property {Animal[]} result
+	 */
+
+	/**
+	 * @typedef BaucuaCache
+	 * @property {'started' | 'ended'} started
+	 * @property {Discord.Snowflake} author
+	 * @property {Bet[]} bets
+	 * @property {number} numberOfRolls
+	 * @property {RollResult[]} rollResults
+	 * @property {string} timeStarted
+	 */
+
+	constructor() {
+		/**
+		 * @type {Animal[]}
+		 */
+		this.animals = ["nai", "bầu", "gà", "cá", "cua", "tôm"];
+		/**
+		 * @type {Discord.Collection<Discord.Snowflake, BaucuaCache>}
+		 */
+		this.baucuaCache = new Discord.Collection();
+	}
+
+	/**
+	 *
+	 * @param {Discord.CommandInteraction} interaction
+	 */
+	async start(interaction) {
+		if (!this.baucuaCache.get(interaction.guild.id)) {
+			this.baucuaCache.set(interaction.guild.id, {
+				started: "started",
+				author: interaction.user.id,
+				bets: [],
+				numberOfRolls: 0,
+				rollResults: [],
+				timeStarted: new Date(interaction.createdTimestamp).toLocaleTimeString(
+					["vi-VN"],
+					{
+						hour: "numeric",
+						minute: "2-digit",
+					}
+				),
+			});
+
+			const text = `<@${interaction.user.id}> đã bắt đầu một game Bầu Cua mới !\nBạn có thể tham gia đặt cược bằng cách: **/baucua bet** !`;
+
+			const embed = new Discord.MessageEmbed()
+				.setColor("WHITE")
+				.setDescription(text)
+				.setTimestamp();
+
+			await interaction.editReply({
+				embeds: [embed],
+			});
+		} else {
+			await interaction.editReply({
+				content: `<@${
+					this.baucuaCache.get(interaction.guild.id).author
+				}> đã bắt đầu một game khác rồi !`,
+				allowedMentions: {
+					users: [],
+				},
+			});
+		}
+	}
+
+	/**
+	 *
+	 * @param {Discord.CommandInteraction} interaction
+	 * @param {Animal} animal
+	 * @param {number} money
+	 */
+	async bet(interaction, animal, money) {
+		if (!this.baucuaCache.get(interaction.guild.id)) {
+			return await interaction.editReply({
+				content: `Vui lòng bắt đầu một game Bầu Cua bằng cách: **/baucua start** !`,
+			});
+		}
+
+		if (!this.animals.includes(animal.toLowerCase())) {
+			return await interaction.editReply({
+				content: `Hãy chọn **Nai**, **Bầu**, **Gà**, **Cá**, **Cua** hoặc **Tôm** !`,
+			});
+		}
+
+		if (isNaN(money)) {
+			return await interaction.editReply({
+				content: `Hãy đưa ra một số tiền phù hợp để đặt cược !`,
+			});
+		}
+
+		if (money < 5000) {
+			return await interaction.editReply({
+				content: `Số tiền đặt cược ít nhất phải là **5.000 VND** !`,
+			});
+		}
+
+		const account = getMoney(interaction.user.id);
+
+		if (!account || account.money < money) {
+			return await interaction.editReply({
+				content: `Bạn chưa có tài khoản hoặc bạn không đủ tiền để đặt cược !`,
+			});
+		}
+
+		const alreadyBet = this.baucuaCache
+			.get(interaction.guild.id)
+			.bets.find(
+				(bet) =>
+					bet.bettedFor === animal.toLowerCase() &&
+					bet.id === interaction.user.id
+			);
+
+		if (alreadyBet) {
+			return await interaction.editReply({
+				content: `Bạn đã đặt cược cho **${
+					animal[0].toUpperCase() + animal.slice(1)
+				}** rồi ! Mỗi người chỉ được đặt cược cho 1 linh vật 1 lần !`,
+			});
+		}
+
+		this.baucuaCache.get(interaction.guild.id).bets.push({
+			id: interaction.user.id,
+			money: money,
+			haveBeenPaidBack: false,
+			win: false,
+			bettedFor: animal,
+		});
+
+		return await interaction.editReply({
+			content: `Bạn đã đặt cược cho **${
+				animal[0].toUpperCase() + animal.slice(1)
+			}** với số tiền là **${money.toLocaleString(["vi-VN"])} VND** !`,
+		});
+	}
+
+	/**
+	 *
+	 * @param {Discord.CommandInteraction} interaction
+	 * @param {Discord.Client} client
+	 */
+	async roll(interaction, client) {
+		const gameSession = this.baucuaCache.get(interaction.guild.id);
+
+		if (!gameSession) {
+			return await interaction.editReply({
+				content: `Vui lòng bắt đầu một game Bầu Cua bằng cách: **/baucua start** !`,
+			});
+		}
+
+		if (interaction.user.id !== gameSession.author) {
+			return await interaction.editReply({
+				content: `Bạn không phải là người host nên không có quyền lăn xúc xắc !`,
+			});
+		}
+
+		if (!gameSession.bets.length) {
+			return await interaction.editReply({
+				content: `Chưa có ai đặt cược ! Hãy đặt cược bằng cách: **/baucua bet** !`,
+			});
+		}
+
+		/**
+		 * @type {Animal[]}
+		 */
+		let result = [];
+
+		for (let i = 0; i < 3; i++) {
+			result.push(
+				this.animals[Math.floor(Math.random() * this.animals.length)]
+			);
+		}
+
+		const numOfRolls = gameSession.numberOfRolls;
+		gameSession.numberOfRolls = numOfRolls + 1;
+		gameSession.rollResults.push({
+			number: numOfRolls + 1,
+			result,
+		});
+
+		const fetchWinners = async () => {
+			/**
+			 * @type {{
+			 *  id: Discord.Snowflake,
+			 *  money: number,
+			 *  haveBeenPaidBack: boolean
+			 * }[]}
+			 */
+			const winnersName = [];
+
+			gameSession.bets.forEach(async (bet) => {
+				if (result.includes(bet.bettedFor)) {
+					bet.win = true;
+					const money = bet.money;
+					const options = {
+						id: bet.id,
+						money,
+						haveBeenPaidBack: true,
+					};
+
+					winnersName.push(options);
+
+					const giveBackOriginalMoney = await addMoney(
+						bet.id,
+						money,
+						getMoney(bet.id).accountId,
+						client,
+						`${client.user.username} tự động thêm tiền do thắng Bầu Cua.`
+					);
+				}
+			});
+
+			result.forEach(async (animal) => {
+				gameSession.bets.forEach(async (bet) => {
+					if (bet.bettedFor === animal.toLowerCase()) {
+						bet.win = true;
+						const money = bet.money;
+
+						const alreadyWon = winnersName.find((e) => e.id === bet.id);
+
+						alreadyWon.money = alreadyWon.money + money;
+
+						const giveBackBonusMoney = await addMoney(
+							bet.id,
+							money,
+							getMoney(bet.id).accountId,
+							client,
+							`${client.user.username} tự động thêm tiền do thắng Bầu Cua.`
+						);
+					}
+				});
+			});
+
+			return winnersName.length
+				? winnersName
+				: [`Không có ai thắng vòng ${numOfRolls + 1} !`];
+		};
+
+		const fetchLosers = async () => {
+			/**
+			 * @type {{
+			 *  id: Discord.Snowflake,
+			 *  money: number,
+			 *  haveBeenPaidBack: boolean
+			 * }[]}
+			 */
+			const losersName = [];
+
+			result.forEach(async (animal) => {
+				gameSession.bets.forEach(async (bet) => {
+					if (bet.win === false && bet.bettedFor !== animal.toLowerCase()) {
+						const money = bet.money;
+
+						if (bet.haveBeenPaidBack === false) {
+							const alreadySubstract = losersName.find((e) => e.id === bet.id);
+
+							if (!alreadySubstract) {
+								losersName.push({
+									id: bet.id,
+									money,
+									haveBeenPaidBack: true,
+								});
+							} else {
+								alreadySubstract.money = alreadySubstract.money + money;
+							}
+
+							const substractFromPlayer = await removeMoney(
+								bet.id,
+								money,
+								getMoney(bet.id).accountId,
+								client,
+								`${client.user.username} tự động trừ tiền do thua Bầu Cua.`
+							);
+
+							const giveToHost = await addMoney(
+								gameSession.author,
+								money,
+								getMoney(bet.id).accountId,
+								client,
+								`${client.user.username} tự động thêm tiền vào tài khoản host do người chơi thua Bầu Cua.`
+							);
+
+							bet.haveBeenPaidBack = true;
+						} else if (bet.haveBeenPaidBack === true) {
+							const substractFromPlayer = await removeMoney(
+								bet.id,
+								money,
+								getMoney(bet.id).accountId,
+								client,
+								`${client.user.username} tự động trừ tiền do thua Bầu Cua.`
+							);
+
+							const giveToHost = await addMoney(
+								gameSession.author,
+								money,
+								getMoney(bet.id).accountId,
+								client,
+								`${client.user.username} tự động thêm tiền vào tài khoản host do người chơi thua Bầu Cua.`
+							);
+
+							const losersFetched = losersName.find(
+								(e) => e.id === bet.id && e.haveBeenPaidBack === true
+							);
+
+							losersFetched.money = losersFetched.money + money;
+						}
+					}
+				});
+			});
+
+			return losersName.length
+				? losersName
+				: [`Không có ai thua vòng ${numOfRolls + 1} !`];
+		};
+
+		const winners = await fetchWinners();
+		const losers = await fetchLosers();
+
+		let generalText = `Kết quả là:\n**${result
+			.map((word) => word[0].toUpperCase() + word.slice(1))
+			.join(
+				" - "
+			)}**\n\nNhững người sau đây đã thắng lần lăn xúc xắc vừa rồi:\n\n`;
+
+		for (const winner of winners) {
+			if (winner === `Không có ai thắng vòng ${numOfRolls + 1} !`) {
+				generalText += `${winner}\n`;
+			} else {
+				generalText += `<@${
+					winner.id
+				}> đã thắng với số tiền **${winner.money.toLocaleString([
+					"vi-VN",
+				])} VND** !\n`;
+			}
+		}
+
+		generalText += `\nNhững người sau đây đã thua lần lăn xúc xắc vừa rồi:\n\n`;
+
+		for (const loser of losers) {
+			if (loser === `Không có ai thua vòng ${numOfRolls + 1} !`) {
+				generalText += `${loser}\n`;
+			} else {
+				generalText += `<@${
+					loser.id
+				}> đã thua với số tiền **${loser.money.toLocaleString([
+					"vi-VN",
+				])} VND** !\n`;
+			}
+		}
+
+		const rollEmbed = new Discord.MessageEmbed()
+			.setTitle(
+				`Kết quả lần lăn xúc xắc thứ ${gameSession.numberOfRolls.toLocaleString(
+					["vi-VN"]
+				)} !`
+			)
+			.setColor("WHITE")
+			.setDescription(generalText)
+			.setFooter({
+				text: `Lần lăn xúc xắc thứ ${gameSession.numberOfRolls.toLocaleString([
+					"vi-VN",
+				])}`,
+			});
+
+		await interaction.editReply({
+			embeds: [rollEmbed],
+		});
+
+		gameSession.bets = [];
+	}
+
+	/**
+	 *
+	 * @param {Discord.CommandInteraction} interaction
+	 */
+	async end(interaction) {
+		const gameSession = this.baucuaCache.get(interaction.guild.id);
+
+		if (!gameSession) {
+			return await interaction.editReply({
+				content: `Chưa có game nào đã bắt đầu để kết thúc !`,
+			});
+		}
+
+		if (interaction.user.id !== gameSession.author) {
+			return await interaction.editReply({
+				content: `Bạn không phải là host nên không có quyền kết thúc game !`,
+			});
+		}
+
+		this.baucuaCache.delete(interaction.guild.id);
+
+		return await interaction.editReply({
+			content: `Đã kết thúc game Bầu Cua do <@${interaction.user.id}> host !`,
+			allowedMentions: {
+				users: {},
+			},
+		});
+	}
+}
+
+module.exports.BauCua = BauCua;
---
+++ features/economy.js
@@ -0,0 +1,331 @@
+const Discord = require("discord.js");
+const profileSchema = require("../schemas/profile-schema");
+const dailyRewardsSchema = require("../schemas/daily-rewards-schema");
+const config = require("../config.json");
+
+/**
+ * @typedef CacheConfig
+ * @property {Discord.Snowflake} accountId
+ * @property {Discord.Snowflake} userId
+ * @property {number} money
+ */
+
+/**
+ * @type {CacheConfig[]}
+ */
+const accountCache = [];
+let claimedCache = [];
+
+const clearCache = () => {
+	claimedCache = [];
+
+	setInterval(clearCache, 1000 * 60 * 10);
+};
+
+async function loadCache() {
+	const results = await profileSchema.find();
+
+	if (!results || !results.length) return;
+
+	for (const result of results) {
+		accountCache.push({
+			accountId: result.accountId,
+			userId: result.userId,
+			money: result.money || 0,
+		});
+	}
+}
+
+/**
+ *
+ * @param {Discord.Snowflake} accountId
+ * @param {number} transactionMoney
+ * @param {number} currentMoney
+ * @param {'add' | 'substract'} addOrSubstract
+ * @param {string} [reason]
+ */
+const generateTransactionText = (
+	accountId,
+	transactionMoney,
+	currentMoney,
+	addOrSubstract,
+	reason = "Không có nội dung."
+) => {
+	const date = new Date().toLocaleTimeString(["vi-VN"], {
+		hour: "numeric",
+		hour12: false,
+		second: "2-digit",
+		minute: "2-digit",
+	});
+
+	return `Tài khoản **${accountId}** đã có giao dịch vào **${date}**\nSố tiền giao dịch: **${
+		addOrSubstract === "add" ? "+" : "-"
+	}${transactionMoney.toLocaleString([
+		"vi-VN",
+	])} VND**\nSố dư hiện tại: **${currentMoney.toLocaleString([
+		"vi-VN",
+	])} VND**\nNội dung giao dịch: **${reason}**`;
+};
+
+const accountIdGenerator = () => {
+	let string = "";
+	const numbers = "1234567890";
+
+	for (let i = 0; i < 12; i++) {
+		string += numbers[Math.floor(Math.random() * numbers.length)];
+	}
+
+	return string;
+};
+
+/**
+ *
+ * @param {Discord.Client} client
+ */
+module.exports = async (client) => {
+	await loadCache();
+	clearCache();
+};
+
+/**
+ *
+ * @param {Discord.Snowflake} userId
+ */
+const createAccount = async (userId) => {
+	const accountId = accountIdGenerator();
+
+	const account = accountCache.find((acc) => acc.userId === userId);
+
+	if (account) return false;
+
+	await new profileSchema({
+		accountId,
+		userId,
+		money: 0,
+	}).save();
+
+	accountCache.push({
+		accountId,
+		userId,
+		money: 0,
+	});
+
+	return accountId;
+};
+
+/**
+ *
+ * @param {Discord.Snowflake} userId
+ */
+const deleteAccount = async (userId) => {
+	const account = accountCache.find((acc) => acc.userId === userId);
+
+	if (!account) return false;
+
+	await profileSchema.findOneAndDelete({
+		userId,
+	});
+
+	await accountCache.splice(accountCache.indexOf(account), 1);
+
+	return true;
+};
+
+/**
+ *
+ * @param {Discord.Snowflake} userId
+ * @param {number} money
+ * @param {Discord.Snowflake} accountId
+ * @param {Discord.Client} client
+ * @param {string} [reason]
+ */
+const addMoney = async (
+	userId,
+	money,
+	accountId,
+	client,
+	reason = "Không có nội dung."
+) => {
+	const account = accountCache.find((acc) =>
+		accountId && accountId.length
+			? acc.accountId === accountId
+			: acc.userId === userId
+	);
+
+	if (!account) {
+		const user = client.users.cache.get(userId);
+
+		return await user.send({
+			embeds: [
+				new Discord.MessageEmbed()
+					.setColor("WHITE")
+					.setDescription(
+						`Bạn hoặc người bạn đã nêu chưa có tài khoản nên bot không thể thêm tiền vào tài khoản đã nêu !\n\nHãy tạo một tài khoản bằng cách: **/account create** !`
+					),
+			],
+		});
+	}
+
+	await profileSchema.findOneAndUpdate(
+		{
+			userId,
+		},
+		{
+			$inc: {
+				money: money,
+			},
+		},
+		{
+			upsert: true,
+		}
+	);
+
+	account.money = account.money + money;
+
+	const user = client.users.cache.get(userId);
+
+	const text = generateTransactionText(
+		accountId,
+		money,
+		account.money,
+		"add",
+		reason
+	);
+
+	return await user.send({
+		embeds: [new Discord.MessageEmbed().setColor("WHITE").setDescription(text)],
+	});
+};
+
+/**
+ *
+ * @param {Discord.CommandInteraction} interaction
+ * @param {Discord.Client} client
+ */
+const daily = async (interaction, client) => {
+	if (claimedCache.includes(interaction.user.id)) {
+		return await interaction.editReply({
+			content: "Bạn đã nhận phần thưởng hằng ngày rồi !",
+		});
+	}
+
+	const obj = {
+		userId: interaction.user.id,
+	};
+
+	const result = await dailyRewardsSchema.findOne(obj);
+
+	if (result) {
+		const then = new Date(result.updatedAt).getTime();
+		const now = new Date().getTime();
+
+		const diffTime = Math.abs(now - then);
+		const diffDays = Math.round(diffTime / (1000 * 60 * 60 * 24));
+
+		if (diffDays <= 1) {
+			claimedCache.push(interaction.user.id);
+
+			return await interaction.editReply({
+				content: "Bạn đã nhận phần thưởng hằng ngày rồi !",
+			});
+		}
+	}
+
+	await dailyRewardsSchema.findOneAndUpdate(obj, obj, {
+		upsert: true,
+	});
+
+	await addMoney(
+		interaction.user.id,
+		config.dailyRewardsMoney,
+		accountCache.find((acc) => acc.userId === interaction.user.id).accountId,
+		client,
+		`Phần thưởng hằng ngày từ ${client.user.username} !`
+	);
+
+	return await interaction.editReply({
+		content: `Bạn đã nhận phần thưởng hằng ngày là **${config.dailyRewardsMoney.toLocaleString(
+			["vi-VN"]
+		)} VND** thành công !`,
+	});
+};
+
+/**
+ *
+ * @param {Discord.Snowflake} userId
+ * @param {Discord.Snowflake} accountId
+ */
+const getMoney = (userId, accountId) => {
+	return accountCache.find((acc) =>
+		accountId && accountId.length > 0
+			? acc.accountId === accountId
+			: acc.userId === userId
+	);
+};
+
+/**
+ *
+ * @param {Discord.Snowflake} userId
+ * @param {number} money
+ * @param {Discord.Snowflake} accountId
+ * @param {Discord.Client} client
+ * @param {string} reason
+ */
+const removeMoney = async (userId, money, accountId, client, reason) => {
+	const account = accountCache.find((acc) =>
+		accountId && accountId.length
+			? acc.accountId === accountId
+			: acc.userId === userId
+	);
+
+	if (!account) {
+		const user = client.users.cache.get(userId);
+
+		return await user.send({
+			embeds: [
+				new Discord.MessageEmbed()
+					.setColor("WHITE")
+					.setDescription(
+						`Bạn hoặc người bạn đã nêu chưa có tài khoản nên bot không thể thêm tiền vào tài khoản đã nêu !\n\nHãy tạo một tài khoản bằng cách: **/account create** !`
+					),
+			],
+		});
+	}
+
+	await profileSchema.findOneAndUpdate(
+		{
+			userId,
+		},
+		{
+			$inc: {
+				money: -money,
+			},
+		},
+		{
+			upsert: true,
+		}
+	);
+
+	account.money = account.money - money;
+
+	const user = client.users.cache.get(userId);
+
+	const text = generateTransactionText(
+		accountId,
+		money,
+		account.money,
+		"substract",
+		reason
+	);
+
+	return await user.send({
+		embeds: [new Discord.MessageEmbed().setColor("WHITE").setDescription(text)],
+	});
+};
+
+module.exports.createAccount = createAccount;
+module.exports.deleteAccount = deleteAccount;
+module.exports.addMoney = addMoney;
+module.exports.daily = daily;
+module.exports.getMoney = getMoney;
+module.exports.removeMoney = removeMoney;

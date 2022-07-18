f32525c1e79b386b8cc1f5441ef6044b7c75ce16
Jason <bennguyen20122006@gmail.com>
2022-07-18 10:45:35 +0700

    dont know how to fix

--- cmd/Utilities/pingmc.js
+++ cmd/Utilities/pingmc.js
@@ -1,6 +1,6 @@
 /*
  * Copyright (C) MeowIce - Mọi quyền được bảo lưu.
- * Tệp này là một phần của dự án MeowAssist. 
+ * Tệp này là một phần của dự án MeowAssist.
  * Nghiêm cấm sao chép trái phép các mã nguồn, tệp tin và thư mục của chương trình này nếu chưa có sự cho phép của chủ sở hữu chương trình - MeowIce.
  */
 
@@ -45,29 +45,42 @@ const commandBase = {
 	},
 	wholeCommand: true,
 	callback: async ({ interaction, client, guild, member, user, options }) => {
-        interaction.deferReply();
-        const address = options.getString("address")
-        let res = await fetch(`https://api.mcsrvstat.us/2/${address}`)
-        let data = res.data
+		interaction.deferReply();
+		const address = options.getString("address");
+		let res = await fetch(`https://api.mcsrvstat.us/2/${address}`);
+		let data = res.data;
 
-        const embed = new MessageEmbed()
-            .setTitle(`Trạng thái của ${address}`)
-            .setColor("RANDOM")
-            .addField(`IP`, data.ip?.toString() || "Không biết", true)
-            .addField(`Port`, data.port?.toString() || "Không biết", true)
-            .addField(`Có SRV`, data.debug.srv?.toString() || "Không biết", true)
-            .addField(`Có IP trong SRV`, data.debug.ipinsrv?.toString() || "Không biết", true)
-            .addField(`Có CNAME trong SRV`, data.debug.cnameinsrv?.toString() || "Không biết", true)
-            .addField(`Phiên bản máy chủ`, data.version?.toString() || "Không biết", true)
-            .addField(`Online`, data.online?.toString() || "Không biết", true)
-            .addField(`Hostname`, data.hostname?.toString() || "Không biết", true)
-            .addField(`Protocol ID`, data.protocol?.toString() || "Không biết", true)
+		const embed = new MessageEmbed()
+			.setTitle(`Trạng thái của ${address}`)
+			.setColor("RANDOM")
+			.addField(`IP`, data.ip?.toString() || "Không biết", true)
+			.addField(`Port`, data.port?.toString() || "Không biết", true)
+			.addField(`Có SRV`, data.debug.srv?.toString() || "Không biết", true)
+			.addField(
+				`Có IP trong SRV`,
+				data.debug.ipinsrv?.toString() || "Không biết",
+				true
+			)
+			.addField(
+				`Có CNAME trong SRV`,
+				data.debug.cnameinsrv?.toString() || "Không biết",
+				true
+			)
+			.addField(
+				`Phiên bản máy chủ`,
+				data.version?.toString() || "Không biết",
+				true
+			)
+			.addField(`Online`, data.online?.toString() || "Không biết", true)
+			.addField(`Hostname`, data.hostname?.toString() || "Không biết", true)
+			.addField(`Protocol ID`, data.protocol?.toString() || "Không biết", true);
 
-        interaction.editReply({
-            embeds: [embed],
-            ephemeral: false
-        }).catch (e => console.log(e))
-        
-    }
-}
+		interaction
+			.editReply({
+				embeds: [embed],
+				ephemeral: false,
+			})
+			.catch((e) => console.log(e));
+	},
+};
 module.exports = commandBase;

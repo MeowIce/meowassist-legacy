fc1b77e9a7ca2124d4f72a9b37c5b792d3ff29a5
Jason <bennguyen20122006@gmail.com>
2022-07-23 14:17:18 +0700

    fixed nick.js and added set and clear subcommands

--- cmd/Utilities/nick.js
+++ cmd/Utilities/nick.js
@@ -4,7 +4,7 @@
  * Nghiêm cấm sao chép trái phép các mã nguồn, tệp tin và thư mục của chương trình này nếu chưa có sự cho phép của chủ sở hữu chương trình - MeowIce.
  */
 
-const { Discord } = require("discord.js");
+const Discord = require("discord.js");
 const { setNick, getBoolean } = require("../../features/nickname");
 const config = require("../../config.json");
 
@@ -38,41 +38,70 @@ const commandBase = {
 			"Để đổi nickname của bạn (hãy đọc luật nickname trước khi sử dụng lệnh này).",
 		options: [
 			{
-				name: "nickname",
-				description: "Biệt danh bạn muốn đổi.",
-				type: "STRING",
-				required: true,
+				name: "set",
+				description: "Để đặt nickname của bạn.",
+				type: "SUB_COMMAND",
+				options: [
+					{
+						name: "nickname",
+						description: "Biệt danh bạn muốn đổi.",
+						type: "STRING",
+						required: true,
+					},
+				],
+			},
+			{
+				name: "clear",
+				description: "Để xoá nickname của bạn.",
+				type: "SUB_COMMAND",
 			},
 		],
 	},
 	wholeCommand: true,
 	callback: async ({ interaction, client, guild, member, user, options }) => {
 		const nickname = options.getString("nickname");
+		const subcommand = options.getSubcommand();
 
-		if (!getBoolean())
-			return await interaction.reply({
-				content:
-					"Tính năng `Đổi biệt danh` đã bị tắt ! Hãy bật nó lên lại để sử dụng !",
-				ephemeral: true,
-			});
+		if (subcommand === "set") {
+			if (!getBoolean())
+				return await interaction.reply({
+					content:
+						"Tính năng `Đổi biệt danh` đã bị tắt ! Hãy bật nó lên lại để sử dụng !",
+					ephemeral: true,
+				});
 
-		if (interaction.channel.id !== config.nicknameChannel) {
-			return await interaction.reply({
-				content: `Bạn vui lòng vào kênh <#${config.nicknameChannel}> để đổi nickname !`,
-				ephemeral: true,
-			});
-		}
+			if (interaction.channel.id !== config.nicknameChannel) {
+				return await interaction.reply({
+					content: `Bạn vui lòng vào kênh <#${config.nicknameChannel}> để đổi nickname !`,
+					ephemeral: true,
+				});
+			}
 
-		const result = await setNick(member, nickname);
+			const result = await setNick(member, nickname);
 
-		if (result.result === false) {
+			if (result.result === false) {
+				return await interaction.reply({
+					content: result.error,
+					ephemeral: true,
+				});
+			} else {
+				return await interaction.reply({
+					content: result.error,
+				});
+			}
+		} else if (subcommand === "clear") {
+			try {
+				await member.setNickname("");
+			} catch (e) {
+				return await interaction.reply({
+					ephemeral: true,
+					content:
+						"Đã có lỗi khi xoá nickname của bạn, vui lòng liên hệ nhân viên đễ được hỗ trợ !",
+				});
+			}
+
 			return await interaction.reply({
-				content: result.error,
-				ephemeral: true,
-			});
-		} else {
-			return await interaction.reply({
-				content: result.error,
+				content: "Đã xoá nickname của bạn thành công !",
 			});
 		}
 	},

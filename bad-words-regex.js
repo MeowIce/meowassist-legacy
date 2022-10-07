3a4b8c94187c40091b07e592d656a250e35f141a
Jason <bennguyen20122006@gmail.com>
2022-10-07 15:33:03 +0700

    fixed nickname.js

---
+++ bad-words-regex.js
@@ -0,0 +1,2 @@
+module.exports =
+	/(cặc|lồn|địt|buồi|cứt|vãi|đít|đụ|đéo|đệch|má|mày|mòe|shit|fuck|penis|dick|d1ck|nigga|n1gga|sex|segg|seggs|chịch|chich|trash|asshole|ass|motherfucker|fucker|sucker|sucks|suck)/gi;

74a18447b30ebe95658fa213a17b7b88bf51b7df
MeowIce <nguyenanhuy2208@gmail.com>
2022-10-22 08:42:32 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,8 +1,13 @@
 # WARNING: Hãy chạy file này lúc khởi động chính thức. KHÔNG được chạy nó khi đang code/debug lỗi.
 # File này sẽ handle exit code 1 or 2 đến từ lệnh shutdown.js và reload.js.
 
+# Xóa thư mục ytdl/ để tiết kiệm bộ nhớ.
 rm -rf ytdl/*.*
+# Tạo lại vì sau khi xóa, fs sẽ không truy cập và tự tạo đc.
+mkdir ytdl/
+# Chạy chương trình :)
 node .
+# Exit code handlers
 if [ $? == "2" ]; then
     while true; do
         echo "User executed reboot command. Restarting..." >&2
